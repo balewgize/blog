f8cf720b0756e20deb7e3ee08cf8cb547c735db2
Alemnew Marie <alemnewmarie461@gmail.com>
2022-10-13 20:18:45 +0300

    Profile picture added

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,9 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("categories/", views.categories, name="categories"),
+    path("tags/", views.tags, name="tags"),
+    path("archives/", views.archives, name="archives"),
+    path("about/", views.about, name="about"),
+    path("post/<slug:slug>/", views.post_detail, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, "blog/index.html")
+
+
+def post_detail(request, slug="post-slug-will-be-passed-from-url"):
+    return HttpResponse("Post detail")
+
+
+def categories(request):
+    return HttpResponse("Categories page")
+
+
+def tags(request):
+    return HttpResponse("Tags page")
+
+
+def archives(request):
+    return HttpResponse("Archives page")
+
+
+def about(request):
+    return HttpResponse("About page")
